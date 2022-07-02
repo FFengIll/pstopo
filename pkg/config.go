c97f746ccf5d72428f295a29b051a405aa8a7f4f
FFengIll <fengyouzheng@gmail.com>
2022-07-02 13:53:43 +0800

    feat: add config dump.

---
+++ pkg/config.go
@@ -0,0 +1,36 @@
+package pkg
+
+import (
+	jsoniter "github.com/json-iterator/go"
+	"io/ioutil"
+)
+
+type Config struct {
+	All  bool     `json:"all"`
+	Cmd  []string `json:"cmd"`
+	Port []uint32 `json:"port"`
+	Pid  []int32  `json:"pid"`
+}
+
+func NewConfig() *Config {
+	return &Config{
+		All:  false,
+		Cmd:  []string{},
+		Port: []uint32{},
+		Pid:  []int32{},
+	}
+}
+
+func (c *Config) WriteTo(path string) {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+
+	data, err := json.Marshal(c)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(path, data, 0555)
+	if err != nil {
+		panic(err)
+	}
+}
